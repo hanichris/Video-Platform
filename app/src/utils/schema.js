5d0db1f34049b4d77b7732eb8327c6a115612f55
hanichris <hanichris71@gmail.com>
2023-04-14 18:07:23 +0300

    Create the usersSchema object.
    Useful in creating the `users` collection to enable validation operations to be undetaken.

--- app/src/utils/schema.js
+++ app/src/utils/schema.js
@@ -1,7 +1,33 @@
 const usersSchema = {
   bsonType: 'object',
   title: 'User Object Validation',
-  required: ['']
+  required: ['name', 'email', 'password'],
+  properties: {
+    name: {
+      bsonType: 'string',
+      description: "'name' must be a string and is required."
+    },
+    email: {
+      bsonType: 'string',
+      description: "'email' must be a string and is required"
+    },
+    password: {
+      bsonType: 'string',
+      description: "'password' must be a string and is required"
+    }
+  }
 };
 
 module.exports = usersSchema;
+
+
+// name of video. description, likes (count), comments,
+// Likes: +1, -1
+// DB: sum(previous, receivedFlag)
+// Associate likes with a userID
+
+// Nature of Comment:
+// Id
+// User whose commented
+// Likes it got 
+// Replies 
