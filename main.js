0cd0621c10cf72e583b1c8eec0721af7820c326b
hanichris <hanichris71@gmail.com>
2023-04-15 17:37:58 +0000

    Test update, get and create functionalities of the dbClient instance.

--- main.js
+++ main.js
@@ -9,20 +9,22 @@ async function listDatabases() {
   });
 }
 
-const user1 = {
-  name: 'Chris',
-  email: 'hanichris71@gmail.com',
-  password: 'slkdjkfljsdlkjlsflkj',
-};
-const user2 = {
-  name: 1234,
-  password: 'sdfkljsldjfl',
-};
+// const user1 = {
+//   name: 'Chris',
+//   email: 'hanichris71@gmail.com',
+//   password: 'slkdjkfljsdlkjlsflkj',
+// };
+// const user2 = {
+//   name: 1234,
+//   password: 'sdfkljsldjfl',
+// };
 
 async function main() {
   try {
     await listDatabases();
-    console.log(await dbClient.createUser(user2));
+    // console.log(await dbClient.createUser(user1));
+    console.log(await dbClient.getUser({ name: 'Chris' }));
+    console.log(await dbClient.updateUser({ name: 'Chris' }, { name: 'Chris Hani' }));
   } catch (error) {
     console.error(error.errInfo.details);
   } finally {
