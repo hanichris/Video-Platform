36bcfb060a883279a9dc9bb673abc72961ccfa8d
hanichris <hanichris71@gmail.com>
2023-04-15 12:50:02 +0000

    Add schema validation while creating the users collection.
    wrap creation in try..catch clause to not repeat creation task.

--- app/src/utils/db.js
+++ app/src/utils/db.js
@@ -1,28 +1,47 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
-import usersSchema from './schema';
 import * as dotenv from 'dotenv';
+import usersSchema from './schema.js';//eslint-disable-line
 
 class DBClient {
   constructor() {
     dotenv.config();
-    this.client = new MongoClient(process.env.DB_CONN_STRING,{
+    this.client = new MongoClient(process.env.DB_CONN_STRING, {
       serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-      }
+      },
     });
-    this.client.connect().then(() => {
-      this.client.db("admin").command({ ping: 1 });
+    this.client.connect().then(async () => {
       console.log('MongoDB client connected to server');
+      this.db = this.client.db(process.env.DB_NAME);
+      try {
+        await this.db.createCollection(process.env.USERS_COLLECTION_NAME, {
+          validator: { $jsonSchema: usersSchema },
+        });
+      } catch (e) {
+        console.error(`Collection ${process.env.DB_NAME}.${process.env.USERS_COLLECTION_NAME} already Exists`);
+      }
     }).catch((err) => console.error(`Mongodb client not connected to the database: ${err}`));
   }
 
-  async getUser(userObject) {}
-  async createUser(userDetails) {}
-  async getVideo() {}
-  async uploadVideo() {}
+  async nbUsers() {
+    const userCount = await this.db.collection(
+      process.env.USERS_COLLECTION_NAME,
+    ).estimatedDocumentCount();
+    return userCount;
+  }
+
+  async createUser(newUser) {
+    const result = await this.db.collection(
+      process.env.USERS_COLLECTION_NAME,
+    ).insertOne(newUser);
+    return result.insertedId;
+  }
+
+  // async getUser(userObject) {}
+  // async getVideo() {}
+  // async uploadVideo() {}
 }
 
-const dbClient = new DBClient();
-module.exports = dbClient;
+export default new DBClient();
