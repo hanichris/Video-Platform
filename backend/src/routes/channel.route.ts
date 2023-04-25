be9d5c3f1c114d3675b4b5048217b64d70d8caf4
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-25 09:12:37 +0300

    Update routes for interacting with streaming, upload and download controllers

--- backend/src/routes/channel.route.ts
+++ backend/src/routes/channel.route.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import ChannelController from "../controllers/ChannelController";
+import UploadController from "../controllers/UploadController"
 import { getAuthToken } from "../middleware/getAuthToken";
 import { requireLogin } from "../middleware/requireLogin";
 
@@ -12,7 +13,7 @@ router.get('/', ChannelController.getChannels); // GET /channels
 router.get('/:id/view', ChannelController.viewChannel); // GET /channels/:id/view
 router.put('/:id', ChannelController.updateChannel); // PUT /channels/:id
 router.post('/', getAuthToken, requireLogin, ChannelController.createChannel); // POST /channels
-router.post('/:id/upload', getAuthToken, requireLogin, ChannelController.uploadVideo); // POST /channels/:id
+router.post('/:id/upload', getAuthToken, requireLogin, UploadController.uploadVideo); // POST /channels/:id
 router.get('/search', ChannelController.search); // GET /channels/search
 router.delete('/:id', getAuthToken, requireLogin, ChannelController.deleteChannel); // DELETE /channels/:id
 
