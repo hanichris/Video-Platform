7d57c3cc7db8e9d8c626299bea36a82bcd6de40c
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-26 00:49:02 +0000

    Update package.json

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -13,7 +13,7 @@ function exclude<User, Key extends keyof User>(
   user: User,
   keys: Key[]
 ): Omit<User, Key> {
-  for (let key in keys) {
+  for (let key of keys) {
     delete user[key];
   }
   return user;
